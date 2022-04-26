efbe7feb0411e7e4b041cf4bdc3a1d4d32682b81
shiloh-byte <57815430+shiloh-byte@users.noreply.github.com>
2022-04-26 18:48:06 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// grab btns and inputs //
+const btn = document.querySelector('.btn');
+const email = document.querySelector("[type='email']");
+const border = document.querySelector('.email');
+const errorMessage = document.querySelector('.input');
+const message = document.createElement('p');
+
+// function for email validation //
+function isValidEmail (string) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    return regex.test(string);
+}
+
+btn.addEventListener('click', function(e) {
+    if (isValidEmail(email.value)) {
+        // remove error styling, add success style //
+        message.classList.remove('error');
+        message.classList.add('success');
+        border.style.border = '1px solid green';
+        message.textContent = 'Email added successfully!';
+        errorMessage.appendChild(message);
+        console.log('success!')
+    } else {
+        // add a class for error styles //
+        message.classList.remove('success');
+        message.classList.add('error');
+        border.style.border = '1px solid red';
+        message.textContent = 'Please provide a valid email address';
+        errorMessage.appendChild(message);
+        console.log('ERROR')
+    }
+})
